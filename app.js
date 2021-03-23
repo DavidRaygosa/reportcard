202104c6de0404fa8acc0c3d48d19fe05805e533
David <david_raygosa@hotmail.com>
2021-03-23 02:22:46 -0600

    Added Limit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var project_routes = require('./routes/routes');
 
 // Middlewares
 
-app.use(bodyParser.urlencoded({extended:true},{limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(bodyParser.json({limit: '50mb'}));
 
 // CORS
